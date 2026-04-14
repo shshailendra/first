b3eedd23a5709eaf3088d3a16a9262b79cbd02c4
shshailendra <sh_shailendra@hotmail.com>
2026-04-14 15:31:26 +0530

    Updated header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,7 +11,7 @@ function Header() {
                 <div className="col-md-3 col-sm-3">
                   <div className="site-title">
                     <a href="/#home" style={{ textDecoration: "none" }}>
-                      <h3> rrrr singh</h3>
+                      <h3> Swagatam Tours</h3>
                     </a>
                   </div>
                 </div>
